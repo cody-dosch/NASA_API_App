8db854b264e35ca2e3c34752fa9cc6ad88db6f3a
guitarman6139@gmail.com <guitarman6139@gmail.com>
2021-08-25 20:38:09 -0400

    Added Earth and Mars Images to Dashboard. Added Fonts and Images to Content.

--- NASA_API_App/App_Start/RouteConfig.cs
+++ NASA_API_App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NASA_API_App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Dashboard", id = UrlParameter.Optional }
             );
         }
     }
