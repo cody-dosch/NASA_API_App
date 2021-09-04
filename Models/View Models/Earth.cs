b38c071d3ad3cde3b08a38470ad7b473c8dd7a9a
guitarman6139@gmail.com <guitarman6139@gmail.com>
2021-09-03 21:43:40 -0400

    Added Models Project containing API Objects and View Models. Added EarthController and Earth.cshtml.

---
+++ Models/View Models/Earth.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.View_Models
+{
+    class Earth
+    {
+        public NASA_NEO_Object NEO { get; set; }
+    }
+}
---
+++ NASA_API_App/Controllers/EarthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+
+namespace NASA_API_App.Controllers
+{
+    public class EarthController : Controller
+    {
+        public ActionResult Earth()
+        {
+            var earthModel = new Models.View_Models.Earth();
+
+            // Put code here to reach out to NASA API and get NEO data, store in earthModel.NEO
+
+            return View(earthModel);
+        }
+    }
+}
